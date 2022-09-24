5ca0d7fc733f678cdceeebc3229bbd6cf4a8f037
Owen <owen.bayliss@sky.com>
2022-09-24 15:57:07 +0100

    Main program added

--- InheritanceSafe/Program.cs
+++ InheritanceSafe/Program.cs
@@ -6,7 +6,11 @@ namespace InheritanceSafe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            SafeOwner owner = new SafeOwner();
+            Safe safe = new Safe();
+            JewelThief jewelThief = new JewelThief();
+            jewelThief.OpenSafe(safe, owner);
+            Console.ReadKey(true);
         }
     }
 }
