a9d4320025efbf364d05f657aa05b614ead0dbab
Owen <owen.bayliss@sky.com>
2022-09-24 15:55:21 +0100

    Classses finished

---
+++ InheritanceSafe/JewelThief.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSafe
+{
+    class JewelThief : Locksmith
+    {
+        private string stolenJewels;
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            stolenJewels = safeContents;
+            Console.WriteLine($"I'm stealing the jewels! I stole: {stolenJewels}");
+        }
+    }
+}
--- InheritanceSafe/Locksmith.cs
+++ InheritanceSafe/Locksmith.cs
@@ -8,5 +8,17 @@ namespace InheritanceSafe
 {
     class Locksmith
     {
+        public void OpenSafe(Safe safe, SafeOwner owner)
+        {
+            safe.PickLock(this);
+            string safeContents = safe.Open(Combination);
+            ReturnContents(safeContents, owner);
+        }
+        public string Combination { private get; set; }
+        protected void ReturnContents(string safeContents, SafeOwner owner)
+        {
+            owner.ReceiveContents(safeContents);
+        }
+
     }
 }
