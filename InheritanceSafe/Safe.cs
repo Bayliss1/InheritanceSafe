606a8ca180e1936b550be8a92239a08547a9e9e1
Owen <owen.bayliss@sky.com>
2022-09-24 15:11:45 +0100

    Add project files.

---
+++ InheritanceSafe/Safe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSafe
+{
+    class Safe
+    {
+        private string contents = "precious jewels";
+        private string safeCombination = "12345";
+
+        public string Open(string combination)
+        {
+            if (combination == safeCombination) return contents;
+            return "";
+        }
+
+        public void PickLock(Locksmith lockpicker)
+        {
+            lockpicker.Combination = safeCombination;
+        }
+    }
+}
---
+++ InheritanceSafe/SafeOwner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceSafe
+{
+    class SafeOwner
+    {
+        private string valuables = "";
+        public void ReceiveContents(string safeContents)
+        {
+            valuables = safeContents;
+            Console.WriteLine($"Thank you for returning my {valuables}!");
+        }
+    }
+}
